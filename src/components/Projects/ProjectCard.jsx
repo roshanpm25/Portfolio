986e874675aa96ab37dd321fd7fa3405b9d25367
roshanpm25 <arsrosh@gmail.com>
2025-05-31 21:24:00 +0530

    Portfolio

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -15,15 +15,7 @@ export const ProjectCard = ({
       />
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
-      <ul className={styles.skills}>
-        {skills.map((skill, id) => {
-          return (
-            <li key={id} className={styles.skill}>
-              {skill}
-            </li>
-          );
-        })}
-      </ul>
+  
       <div className={styles.links}>
 
       </div>
